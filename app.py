62632c1f6933d38c4c1ae61edf6901ca7ce724d9
98abhilash <98abhilash@gmail.com>
2020-05-29 15:57:47 +0530

    Error Resolved

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-dat
 @st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows = nrows)
+    st.write(data)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase,axis='columns',inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
@@ -27,6 +28,8 @@ if st.checkbox('Show raw data'):
 
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+
 st.bar_chart(hist_values)
 
 # Some number in the range 0-23
