20b84e731f6ff264084cfd89c9f97d27704b6d17
98abhilash <98abhilash@gmail.com>
2020-05-29 15:58:33 +0530

    Data Fetch Error

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ if st.checkbox('Show raw data'):
 
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 
 st.bar_chart(hist_values)
 
